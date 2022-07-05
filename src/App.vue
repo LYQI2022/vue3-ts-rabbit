eacd10f1b503016c49a97487c56f9fcfc7af3651
liuyuqi <1298386145@qq.com>
2022-07-05 11:27:09 +0800

    feat:rabbit init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    App 组件
+  </div>
+</template>
+
+<script setup lang="ts">
+import axios from '@/utils/request'
+axios.get('/home/index').then(res => console.log(res)
+)
+
+</script>
+
+<style scoped>
+
+</style>
